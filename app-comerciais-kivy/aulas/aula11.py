564310e465e8a87fc7132a37b4e4f707d11c0d90
Diogo Paza <diogopazacvel@gmail.com>
2018-09-16 22:16:27 -0300

    exercicios

---
+++ app-comerciais-kivy/aulas/aula11.py
@@ -0,0 +1,15 @@
+#OPERADORES LÓGICOS
+
+"""
+print(100 == 100)
+print('a' == 'b')
+print('a' != 'b')
+
+a= ( 200 == 201)
+print(a)
+"""
+a = 5.111
+b= 10*'-'
+c= 2 + .1
+d=int("2")
+print( type(d) )
---
+++ app-comerciais-kivy/aulas/exercicios.py
@@ -0,0 +1,39 @@
+"""""
+#1
+print('Diogo Paza')
+
+#2
+nome = input('Digite seu nome')
+print("O seu nome é %s" %nome)
+
+nome= input('Digite seu nome')
+idade = input('Digite sua idade')
+print("Seu nome é %s e sua idade é de %s" %(nome, idade))
+
+
+numero = input("Digite um numero")
+print(numero)
+
+num = int(input('Digite um numero'))
+print('O número digitado foi: %i' %num)
+
+num1 = int(input('Digite um numero'))
+num2 = int(input('Digite um numero'))
+num3 = int(input('Digite um numero'))
+print( num1 + num2 + num3 )
+
+
+num1 = int(input('Digite um numero'))
+num2 = int(input('Digite um numero'))
+soma = num1 + num2
+print("a soma entre %i e %i é igual %i" %(num1, num1, soma) )
+"""
+
+nota1 = float(input("Digite a primeira nota"))
+nota2 = float(input("Digite a segunda nota"))
+nota3 = float(input("Digite a terceira nota"))
+nota4 = float(input("Digite a quarta nota"))
+media = ( nota1 + nota2 + nota3 + nota4) / 4
+print("A média é: %f" %media)
+
+
